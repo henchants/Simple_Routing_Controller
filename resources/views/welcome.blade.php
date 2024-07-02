3081daef64864b69081c96bda9813fde61360bb8
Hen <henchants@gmail.com>
2024-07-02 11:43:49 +0800

    replaced links for welcome.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 
-    <a href=https://github.com/henchants/Simple_Routing_Controller/tree/main/app><button>about Me</button></a>
-    <a href=https://github.com/henchants/Simple_Routing_Controller/tree/main/app><button>skills</button></a>
-    <a href=https://github.com/henchants/Simple_Routing_Controller/tree/main/app><button>Hobbies</button></a>
+    <a href=https://github.com/henchants/Simple_Routing_Controller/blob/main/resources/views/about_me.blade.php><button>about Me</button></a>
+    <a href=https://github.com/henchants/Simple_Routing_Controller/blob/main/resources/views/skills.blade.php><button>skills</button></a>
+    <a href=https://github.com/henchants/Simple_Routing_Controller/blob/main/resources/views/hobbies.blade.php><button>Hobbies</button></a>
 </body>
 </html>
