bbd68c1f797d1245d0d682a9fde8870aed4be4c3
Hen <henchants@gmail.com>
2024-07-02 11:42:48 +0800

    edited resources/views

--- resources/views/about_me.blade.php
+++ resources/views/about_me.blade.php
@@ -5,11 +5,6 @@
 </head>
 <body>
     <h1>Skills</h1>
-    <ul>
-        @foreach ($skills as $skill)
-            <li>{{ $skill }}</li>
-        @endforeach
-    </ul>
 
     <a href="{{ route('about.me') }}">About Me</a>
     <a href="{{ route('skills') }}">Skills</a>
--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -5,12 +5,6 @@
 </head>
 <body>
     <h1>Hobbies</h1>
-    <ul>
-        @foreach ($hobbies as $hobby)
-            <li>{{ $hobby }}</li>
-        @endforeach
-    </ul>
-
     <a href="{{ route('about.me') }}">About Me</a>
     <a href="{{ route('skills') }}">Skills</a>
     <a href="{{ route('hobbies') }}">Hobbies</a>
